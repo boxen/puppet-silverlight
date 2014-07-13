f21f41761dcb3d761250d1e54727725dd88efcf1
Nick Pellant <nick@nickpellant.com>
2014-07-13 12:11:00 +0100

    Update silverlight source.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,6 @@ class silverlight {
   package { 'Silverlight':
     ensure   => 'installed',
     provider => 'pkgdmg',
-    source   => 'http://go.microsoft.com/fwlink/?LinkID=229322',
+    source   => 'http://silverlight.dlservice.microsoft.com/download/D/6/6/D66CF013-1021-437B-9A65-983871CCB3E6/30317.00/Silverlight.dmg',
   }
 }
